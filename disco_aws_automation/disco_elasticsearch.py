5aaa381c22a25f3b33f462323c7f91fbd709260d
isukhanov <isukhanov@wgen.net>
2016-03-30 17:16:45 -0400

    Force mapping between env and es cluster

--- disco_aws_automation/disco_elasticsearch.py
+++ disco_aws_automation/disco_elasticsearch.py
@@ -21,7 +21,7 @@ class DiscoES(object):
     A simple class to manage ElasticSearch
     """
 
-    def __init__(self, config_file='disco_elasticsearch.ini', aws=None):
+    def __init__(self, config_file='disco_elasticsearch.ini', aws):
         self.conn = boto3.client('es')
         self.config_file = config_file
         self._config = None  # lazily initialized
@@ -46,122 +46,81 @@ class DiscoES(object):
         return sorted([domain['DomainName'] for domain in
                        response['DomainNames']])
 
-   #def create(self, domain_name):
-   #    """
-   #    Create a new elasticsearch domain based on the config file
-   #    """
-
-   #    logging.info('Creating elasticsearch domain %s...', domain_name)
-   #    throttled_call(self.conn.create_elasticsearch_domain, DomainName=domain_name)
-   #    logging.info('Creating elasticsearch domain %s will take approx. 10 minutes', domain_name)
-
-   #    return None
-
-    def delete(self, domain_name):
+    def delete(self):
         """
         Delete an elasticsearch domain
-        Args:
-            domain_name (str): name of elasticsearch domain
         """
         domains = self.list()
 
-        if domain_name not in domains:
-            logging.info('Elasticsearch domain %s does not exist. Nothing to delete.', domain_name)
+        if self._cluster_name not in domains:
+            logging.info('Elasticsearch domain %s does not exist. Nothing to delete.', self._cluster_name)
             return
 
-        logging.info('Deleting elasticsearch domain %s', domain_name)
+        logging.info('Deleting elasticsearch domain %s', self._cluster_name)
         throttled_call(self.conn.delete_elasticsearch_domain,
-                       DomainName=domain_name)
+                       DomainName=self._cluster_name)
 
-    def _describe_es_domain(self, domain_name):
+    def _describe_es_domain(self):
         """
         Returns domain configuration information about the specified
         Elasticsearch domain, including the domain ID, domain endpoint, and
         domain ARN.
         """
-        return self.conn.describe_elasticsearch_domain(DomainName=domain_name)
+        return self.conn.describe_elasticsearch_domain(DomainName=self._cluster_name)
 
-    # pylint: disable=too-many-arguments
-    #def _create_es_domain(self, domain_name, instance_type, instance_count,
-    def create(self, domain_name, instance_type, instance_count,
-                          dedicated_master, zone_awareness, dedicated_master_type,
-                          dedicated_master_count, ebs_enabled, volume_type,
-                          volume_size, iops, access_policies, snapshot_start_hour):
-        """
-        Create an elasticsearch domain
+    @property
+    def _cluster_name(self):
+	return "{0}-log-es".format(aws.environment_name)
 
-        Args:
-            domain_name (str): name of elasticsearch domain
-            instance_type (str): instance types, use instance types that end with '.elasticsearch'
-            instance_count (int): number of instances in the specified domain cluster
-            dedicated_master (boolean): indicate whether a dedicated master node is enabled
-            zone_awareness (boolean): indicate whether zone awareness is enabled
-            dedicated_master_type (string): instance type for a dedicated master node
-            dedicated_master_count (int): total number of dedicated master nodes for the cluster
-            ebs_enabled (boolean): whether EBS-based storage is enabled
-            volume_type (string): volume type for EBS-based storage
-            volume_size (int): specify the size of an EBS volume
-            iops (int): specify IOPD for a Provisioned IOPS EBS volume (SSD)
-            access_policies (string): IAM access policy as a JSON-formatted string
-            snapshot_start_hour (int): time, in UTC format, when the service takes a daily automated snapshot
-        """
+    def _access_policy(self):
+	disco_iam = DiscoIAM(
+		environment=self.aws.environment_name,
+		boto2_connection=self.aws.connection
+	)
+	return '''
+	    {
+	      "Version": "2012-10-17",
+	      "Statement": [
+		{
+		  "Effect": "Allow",
+		  "Principal": {
+		    "AWS": [
+		      "{1}"
+		    ]
+		  },
+		  "Action": [
+		    "es:*"
+		  ],
+		  "Resource": "arn:aws:es:{0}:{1}:domain/{2}/*"
+		}
+	      ]
+	    }
+	'''.format(
+                self.aws.vpc.region,
+                disco_iam.account_id(),
+                self._cluster_name,
+	)
 
-        throttled_call(self.conn.create_elasticsearch_domain,
-                       DomainName=domain_name,
-                       InstanceType=instance_type,
-                       InstanceCount=instance_count,
-                       DedicatedMasterEnabled=dedicated_master,
-                       ZoneAwarenessEnabled=zone_awareness,
-                       DedicatedMasterType=dedicated_master_type,
-                       DedicatedMasterCount=dedicated_master_count,
-                       EBSEnabled=ebs_enabled,
-                       VolumeType=volume_type,
-                       VolumeSize=volume_size,
-                       Iops=iops,
-                       AccessPolicies=access_policies,
-                       AutomatedSnapshotStartHour=snapshot_start_hour)
+    def create(self):
+        return self._upsert(self.conn.create_elasticsearch_domain)
 
-        return None
+    def update(self):
+        return self._upsert(self.conn.update_elasticsearch_domain_config)
 
-    #  pylint: disable=too-many-arguments
-    def _update_es_domain(self, domain_name, instance_type, instance_count,
-                          dedicated_master, zone_awareness, dedicated_master_type,
-                          dedicated_master_count, ebs_enabled, volume_type,
-                          volume_size, iops, snapshot_start_hour,
-                          access_policies):
-        """
-        Modifies the cluster configuration of the specified Elasticsearch domain,
-        setting as setting the instance type and the number of instances.
-
-        Args:
-            domain_name (str): name of elasticsearch domain
-            instance_type (str): instance types, use instance types that end with '.elasticsearch'
-            instance_count (int): number of instances in the specified domain cluster
-            dedicated_master (boolean): indicate whether a dedicated master node is enabled
-            zone_awareness (boolean): indicate whether zone awareness is enabled
-            dedicated_master_type (string): instance type for a dedicated master node
-            dedicated_master_count (int): total number of dedicated master nodes for the cluster
-            ebs_enabled (boolean): whether EBS-based storage is enabled
-            volume_type (string): volume type for EBS-based storage
-            volume_size (int): specify the size of an EBS volume
-            iops (int): specify IOPD for a Provisioned IOPS EBS volume (SSD)
-            snapshot_start_hour (int): time, in UTC format, when the service takes a daily automated snapshot
-            access_policies (string): IAM access policy as a JSON-formatted string
-        """
-
-        throttled_call(self.conn.create_elasticsearch_domain,
-                       DomainName=domain_name,
-                       InstanceType=instance_type,
-                       InstanceCount=instance_count,
-                       DedicatedMasterEnabled=dedicated_master,
-                       ZoneAwarenessEnabled=zone_awareness,
-                       DedicatedMasterType=dedicated_master_type,
-                       DedicatedMasterCount=dedicated_master_count,
-                       EBSEnabled=ebs_enabled,
-                       VolumeType=volume_type,
-                       VolumeSize=volume_size,
-                       Iops=iops,
-                       AccessPolicies=access_policies,
-                       AutomatedSnapshotStartHour=snapshot_start_hour)
-
-        return None
+    def _upsert(self, generator):
+        throttled_call(
+           generator,
+           DomainName=self._cluster_name,
+           InstanceType=self.aws.vpc.get_config("es_instance_type", "extra_small"),
+           InstanceCount=instance_count,
+           DedicatedMasterEnabled=dedicated_master,
+           ZoneAwarenessEnabled=zone_awareness,
+           DedicatedMasterType=dedicated_master_type,
+           DedicatedMasterCount=dedicated_master_count,
+           EBSEnabled=ebs_enabled,
+           VolumeType=volume_type,
+           VolumeSize=volume_size,
+           Iops=iops,
+           AccessPolicies=self._access_policy(),
+           AutomatedSnapshotStartHour=snapshot_start_hour
+        )
