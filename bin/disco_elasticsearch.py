ea91f2e3a041ae38c92e82ad9e0e001f69feae32
Keli Bezhani <ebezhani@amplify.com>
2016-03-30 16:15:55 -0400

    Fixed some pep8 and pylint

--- bin/disco_elasticsearch.py
+++ bin/disco_elasticsearch.py
@@ -22,9 +22,9 @@ Options:
     --domain ES_DOMAIN  Name of elasticsearch domain
 """
 from __future__ import print_function
-import sys
+# import sys
 from docopt import docopt
-from disco_aws_automation import DiscoES#, DiscoAWS, read_config
+from disco_aws_automation import DiscoES, read_config  # , DiscoAWS
 from disco_aws_automation.disco_aws_util import run_gracefully
 from disco_aws_automation.disco_logging import configure_logging
 
@@ -35,26 +35,29 @@ def run():
 
     configure_logging(args["--debug"])
 
-    #config = read_config()
+    config = read_config('../asiaq_astro_config/disco_elasticsearch.ini')
 
-    #aws = DiscoAWS(config, env)
-    #disco_elasticsearch = DiscoES(aws=aws)
+    # aws = DiscoAWS(config, env)
+    # disco_elasticsearch = DiscoES(aws=aws)
     disco_elasticsearch = DiscoES()
 
     if args['list']:
         for domain in disco_elasticsearch.list():
             if args['--endpoint']:
                 try:
-                    endpoint = disco_elasticsearch._describe_es_domain(domain)\
-                    ['DomainStatus']['Endpoint']
+                    endpoint = disco_elasticsearch._describe_es_domain(domain)['DomainStatus']['Endpoint']
                 except KeyError:
                     endpoint = None
-                print('{0:20}\t{1}'.format(domain,endpoint))
+                print('{0:20}\t{1}'.format(domain, endpoint))
             else:
                 print(domain)
 
     elif args['create']:
-        disco_elasticsearch.create(args['--domain'])
+        if config.items(args['--domain']):
+            print(config.items(args['--domain']))
+            disco_elasticsearch.create(args['--domain'], [item[1] for item in config.items(args['--domain'])])
+        else:
+            disco_elasticsearch.create(args['--domain'])
 
     elif args['update']:
         disco_elasticsearch.update(args['--domain'])
