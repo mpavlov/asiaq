a429cf6b9470ad36daa71526c1d4dd0e88b79820
Johnathan Constance <jconstance@amplify.com>
2016-04-27 08:58:45 -0400

    Rename this argument to make this logic slightly clearer

--- bin/disco_elasticsearch.py
+++ bin/disco_elasticsearch.py
@@ -31,7 +31,7 @@ def get_parser():
                                           "default behavior is to update all ElasticSearch domains found in "
                                           "the config.")
     parser_update.set_defaults(mode="update")
-    parser_update.add_argument("--name", dest="name", type=str, action="append",
+    parser_update.add_argument("--name", dest="names", type=str, action="append",
                                help="Name of the ElasticSearch domain")
 
     parser_delete = subparsers.add_parser("delete",
@@ -39,7 +39,7 @@ def get_parser():
                                           "default behavior is to delete all ElasticSearch domains found in "
                                           "the config.")
     parser_delete.set_defaults(mode="delete")
-    parser_delete.add_argument("--name", dest="name", type=str, action="append",
+    parser_delete.add_argument("--name", dest="names", type=str, action="append",
                                help="Name of the ElasticSearch domain")
     parser_delete.add_argument("--all", dest="delete_all", action='store_const', default=False, const=True,
                                help="Delete *all* ElasticSearch domains")
@@ -70,17 +70,17 @@ def run():
                 values.append(entry["elasticsearch_endpoint"] or u"-")
             print(format_line.format(*values))
     elif args.mode == "update":
-        if args.name:
-            for name in args.name:
+        if args.names:
+            for name in args.names:
                 disco_es.update(name)
         else:
             disco_es.update()
     elif args.mode == "delete":
         prompt = "Deleting an ElasticSearch domain destroys all of its automated snapshots. Be careful!\n"
-        if args.name:
+        if args.names:
             prompt += "Are you sure you want to delete ElasticSearch domains {}? (y/N)".format(args.name)
             if not interactive_shell or is_truthy(raw_input(prompt)):
-                for name in args.name:
+                for name in args.names:
                     disco_es.delete(name)
         else:
             scope = "all configured" if not args.delete_all else "*all*"
