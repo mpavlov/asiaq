30e1f6ca2224cba216c2e08f2600ae55ba43cebb
Sejin Kim <skim@amplify.com>
2016-04-29 14:53:55 -0400

    update comment

--- test/unit/test_disco_aws_util.py
+++ test/unit/test_disco_aws_util.py
@@ -10,23 +10,24 @@ class DiscoAWSUtilTests(TestCase):
     '''Test disco_aws_util.py'''
 
     def test_size_as_rec_map_with_none(self):
-        """_size_as_recurrence_map works with None"""
+        """size_as_recurrence_map works with None"""
         self.assertEqual(disco_aws_util.size_as_recurrence_map(None), {"": None})
         self.assertEqual(disco_aws_util.size_as_recurrence_map(''), {"": None})
 
     def test_size_as_rec_map_with_int(self):
-        """_size_as_recurrence_map works with simple integer"""
+        """size_as_recurrence_map works with simple integer"""
         self.assertEqual(disco_aws_util.size_as_recurrence_map(5, sentinel="0 0 * * *"),
                          {"0 0 * * *": 5})
 
     def test_size_as_rec_map_with_map(self):
-        """_size_as_recurrence_map works with a map"""
+        """size_as_recurrence_map works with a map"""
         map_as_string = "2@1 0 * * *:3@6 0 * * *"
         map_as_dict = {"1 0 * * *": 2, "6 0 * * *": 3}
         self.assertEqual(disco_aws_util.size_as_recurrence_map(map_as_string), map_as_dict)
 
     def test_size_as_rec_map_with_duped_map(self):
-        """_size_as_recurrence_map works with a duped map"""
+        """size_as_recurrence_map works with a duped map"""
         map_as_string = "2@1 0 * * *:3@6 0 * * *:3@6 0 * * *"
         map_as_dict = {"1 0 * * *": 2, "6 0 * * *": 3}
         self.assertEqual(disco_aws_util.size_as_recurrence_map(map_as_string), map_as_dict)
+
