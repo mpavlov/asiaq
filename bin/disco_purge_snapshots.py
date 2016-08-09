d1949535052b974d4ad8746d944dfee24ec449da
Alex Oskotsky <aoskotsky@amplify.com>
2016-08-09 00:26:17 -0400

    added unit tests for disco_purge_snapshots. added --keep-days option
    --keep-days deprecates the old --old option. --keep-days allows passing in a number of days instead of harcoding to 100

--- bin/disco_purge_snapshots.py
+++ bin/disco_purge_snapshots.py
@@ -15,13 +15,14 @@ Usage:
     disco_purge_snapshots.py [options]
 
 Options:
-    -h --help          Show this screen.
-    --debug            Log in debug level.
-    --stray-ami        Purge only snapshots created by CreateImage
-    --no-metadata      Purge only snapshots with no tags
-    --old              Purge only old snapshots (100 days)
-    --keep NUM_TO_KEEP Keep at least this number of snapshots per hostclass per environment
-    --dry-run          Only print what will be done
+    -h --help                 Show this screen.
+    --debug                   Log in debug level.
+    --stray-ami               Purge only snapshots created by CreateImage
+    --no-metadata             Purge only snapshots with no tags
+    --old                     Purge only old snapshots (100 days) [DEPRECATED: use --keep-days instead]
+    --keep-days DAYS          Delete snapshots older than this number of days
+    --keep-num NUM            Keep at least this number of snapshots per hostclass per env
+    --dry-run                 Only print what will be done
 """
 
 from __future__ import print_function
@@ -40,6 +41,8 @@ from disco_aws_automation.disco_aws_util import run_gracefully
 from disco_aws_automation.disco_logging import configure_logging
 
 OLD_IMAGE_DAYS = 100
+DEFAULT_KEEP_LAST = 5
+NOW = datetime.now(pytz.UTC)
 
 
 def run():
@@ -51,10 +54,11 @@ def run():
     configure_logging(args["--debug"])
 
     # If no options are set, we assume user wants all of 'em.
-    arg_options = ["--stray-ami", "--no-metadata", "--old"]
+    arg_options = ["--stray-ami", "--no-metadata",
+                   "--keep-days", OLD_IMAGE_DAYS,
+                   "--keep-num", DEFAULT_KEEP_LAST]
     if not any([args[option] for option in arg_options if option in args]):
-        for option in arg_options:
-            args[option] = True
+        args = docopt(__doc__, argv=arg_options)
 
     _ignore, failed_to_purge = purge_snapshots(args)
     if failed_to_purge:
@@ -63,14 +67,13 @@ def run():
 
 def purge_snapshots(options):
     """
-    Purge snapshots we concider no longer worth keeping
+    Purge snapshots we consider no longer worth keeping
     """
     ec2_conn = boto.connect_ec2()
     snap_pattern = re.compile(
         r"Created by CreateImage\(i-[a-f0-9]+\) for ami-[a-f0-9]+"
     )
     ami_pattern = re.compile(r"ami-[a-f0-9]+")
-    now = datetime.now(pytz.UTC)
 
     images_by_id = {
         image.id: image for image in ec2_conn.get_all_images(owners=['self'])
@@ -96,25 +99,27 @@ def purge_snapshots(options):
             continue
 
         # build a dict of hostclass+environment to a list of snapshots
-        # use this dict for the --keep option to know how many snapshots are there for each hostclass
+        # use this dict for the --keep-num option to know how many snapshots are there for each hostclass
         if snap.tags and snap.tags.get('hostclass') and snap.tags.get('env'):
             key_name = snap.tags.get('hostclass') + '_' + snap.tags.get('env')
             hostclass_snapshots = snapshot_dict.setdefault(key_name, [])
             hostclass_snapshots.append(snap)
 
-        if options["--old"]:
+        if options["--old"] or options["--keep-days"]:
+            old_days = int(options.get('--keep-days') or OLD_IMAGE_DAYS)
+
             snap_date = iso8601.parse_date(snap.start_time)
-            snap_days_old = (now - snap_date).days
-            if snap_days_old > OLD_IMAGE_DAYS:
-                print("Deleting old ({1} days) snapshot: {0}".format(
-                    snap.id, snap_days_old))
+            snap_days_old = (NOW - snap_date).days
+            if snap_days_old > old_days:
+                print("Deleting old ({1} > {2} days) snapshot: {0}".format(
+                    snap.id, snap_days_old, old_days))
                 snaps_to_purge.append(snap)
                 continue
 
         logging.debug("skipping snapshot: %s description: %s tags: %s", snap.id, snap.description, snap.tags)
 
-    if options.get('--keep'):
-        snaps_to_purge = remove_kept_snapshots(snaps_to_purge, int(options.get('--keep')), snapshot_dict)
+    if options.get('--keep-num'):
+        snaps_to_purge = remove_kept_snapshots(snaps_to_purge, int(options.get('--keep-num')), snapshot_dict)
 
     if not options["--dry-run"]:
         for snap in snaps_to_purge:
@@ -133,7 +138,7 @@ def remove_kept_snapshots(snaps_to_purge, keep_count, snapshot_dict):
     snapshots are kept for each hostclass in each environment
     """
     if keep_count < 1:
-        raise ValueError("The number of snapshots to keep must be greater than 1 when specifying --keep")
+        raise ValueError("The number of snapshots to keep must be greater than 1 for --keep-num")
     snaps_to_keep = []
     for hostclass_snapshots in snapshot_dict.values():
         keep_for_hostclass = sorted(hostclass_snapshots,
@@ -144,9 +149,9 @@ def remove_kept_snapshots(snaps_to_purge, keep_count, snapshot_dict):
         hostclass = keep_for_hostclass[0].tags['hostclass']
         env = keep_for_hostclass[0].tags['env']
 
-        logging.debug(
-            "Keeping last %s snapshots (%s) for hostclass %s in environment %s",
-            keep_count, snap_ids, hostclass, env
+        print(
+            "Keeping last %s snapshots (%s) for hostclass %s in environment %s" %
+            (keep_count, snap_ids, hostclass, env)
         )
 
     # remove the snapshots we plan to keep from purge list
